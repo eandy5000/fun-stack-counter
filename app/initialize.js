5a076092981fb0b22a6309cb22cf14fa8272d5bb
Andy Eccleston <eandy5000@yahoo.com>
2018-03-12 20:56:45 -0500

    rebuilt counter for practice

--- app/initialize.js
+++ app/initialize.js
@@ -3,40 +3,40 @@ import hh from "hyperscript-helpers";
 import R from "ramda";
 
 const tags = hh(h);
-const { div, button } = tags;
+const { div, button, h3 } = tags;
 
 document.addEventListener("DOMContentLoaded", () => {
   // do your setup here
-  const initModel = 0;
+  const initalModel = 0;
   const root = document.getElementById("root");
   //view function
   function view(dispatch, model) {
-    return div(
-      div(`Counter: ${model}`),
-      button("+", { onclick: () => dispatch("plus") }),
-      button("-", { onclick: () => dispatch("minus") }),
-      button("reset", { onclick: () => dispatch("reset") })
-    );
+    return div({ className: "ma3" }, [
+      h3({ className: "ml3" }, model),
+      button({ onclick: () => dispatch("plus") }, "+"),
+      button({ onclick: () => dispatch("minus"), className: "ml3" }, "-"),
+      button({ onclick: () => dispatch("reset"), className: "ml3" }, "reset")
+    ]);
   }
   //update function
   function update(msg, model) {
     switch (msg) {
-      case "plus":
-        return model + 1;
       case "minus":
         return model - 1;
+      case "plus":
+        return model + 1;
       case "reset":
         return (model = 0);
       default:
         return model;
     }
   }
-
   //impure app function
-  function app(initModel, update, view, node) {
-    let model = initModel;
+  function app(initalModel, update, view, node) {
+    let model = initalModel;
     let currentView = view(dispatch, model);
     node.appendChild(currentView);
+
     function dispatch(msg) {
       model = update(msg, model);
       const updatedView = view(dispatch, model);
@@ -45,5 +45,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  app(initModel, update, view, root);
+  app(initalModel, update, view, root);
 });
